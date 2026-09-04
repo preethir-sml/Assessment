daa4d91872d3d888c55becf6863531ed15db75af
Preethi <preethirstr@gmail.com>
2026-09-04 15:31:55 +0530

    updated delete method

--- server.js
+++ server.js
@@ -15,27 +15,26 @@ app.get('/tasks/:id', (req, res) => {
     const taskid = Number(req.params.id);
     const id = tasklist.find(n => n.id === taskid);
     if (!id) {
-        res.status(404).json({ message: "Task not found" })
+        res.status(404).json({ message: "Task not found" });
     }
-    res.json(id);
-
+    res.json(tasklist[id]);
 });
 app.post('/tasks', (req, res) => {
-    const { task, status } = req.body;
+    const { id, task, status } = req.body;
     const newTask = {
-        id: tasklist.length + 1,
+        id,
         task,
         status
     };
-    if (!id || !task || !status) {
+    if (!task || !status) {
         res.json({ message: "Id , Task and Status is required" })
     }
-    tasklist.push(newTask)
+    tasklist.push(newTask);
     return res.status(201).json({ message: "Successfully created" });
 });
 app.delete('/tasks/:id', (req, res) => {
     const id = Number(req.params.id);
-    tasklist.filter(task => task.id === tasklist.id);
+    tasklist = tasklist.filter(task => task.id !== id);
     res.status(200).json({ message: "Successfully deleted" })
 });
 app.listen(3000, () => {
